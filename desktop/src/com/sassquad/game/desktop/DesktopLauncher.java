895ad6f711c802ce9776781f304888b845b31b79
Ivan Ristov <ristovi@CNU339BKYT.corp.auspost.local>
2015-04-16 16:47:38 +1000

    new repo

---
+++ desktop/src/com/sassquad/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sassquad.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sassquad.game.PRGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PRGame(), config);
+	}
+}
